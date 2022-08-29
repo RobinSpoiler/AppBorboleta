a876f17088f271b94272309ab424f87fc47e3974
Jorge Delgado <jorgeadm4@gmail.com>
2022-08-28 19:10:20 -0700

    Reorganized files into MVC model

--- ArboletaIOS/ArboletaIOS/Controllers/StartPageViewController.swift
+++ ArboletaIOS/ArboletaIOS/Controllers/StartPageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartPageViewController: UIViewController {
 
     @IBOutlet weak var personCard1: UIImageView!
     @IBOutlet weak var personCard2: UIImageView!
