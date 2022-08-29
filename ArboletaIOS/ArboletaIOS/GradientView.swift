fdea96ed7cfbbecf9e96b42a8e7e6a6a6378b247
Jorge Delgado <jorgeadm4@gmail.com>
2022-08-28 18:28:27 -0700

    Added start page

---
+++ ArboletaIOS/ArboletaIOS/GradientView.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  ArboletaIOS
+//
+//  Created by Jorge Delgado on 27/08/22.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var FirstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    @IBInspectable var SecondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [ FirstColor.cgColor, SecondColor.cgColor ]
+    }
+}
--- ArboletaIOS/ArboletaIOS/ViewController.swift
+++ ArboletaIOS/ArboletaIOS/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var personCard1: UIImageView!
+    @IBOutlet weak var personCard2: UIImageView!
+    @IBOutlet weak var personCard3: UIImageView!
+    @IBOutlet weak var personCard4: UIImageView!
+    @IBOutlet weak var personCard5: UIImageView!
+    @IBOutlet weak var personCard6: UIImageView!
+    @IBOutlet weak var personCard7: UIImageView!
+    @IBOutlet weak var personCard8: UIImageView!
+    @IBOutlet weak var personCard9: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let personCards = [
+            personCard1,
+            personCard2,
+            personCard3,
+            personCard4,
+            personCard5,
+            personCard6,
+            personCard7,
+            personCard8,
+            personCard9
+        ]
+        
+        // add rounded corners, and a white border
+        for person in personCards {
+            person!.layer.cornerRadius = (person?.frame.size.height)! / 10
+            person!.layer.borderWidth = (person?.frame.size.height)! / 80
+            person!.layer.borderColor = UIColor.white.cgColor
+        }
     }
-
-
 }
-
