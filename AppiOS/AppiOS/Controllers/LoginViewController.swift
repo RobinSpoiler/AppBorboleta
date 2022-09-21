786f6af8c08ec1f9e43744d8b2e4814ecee19502
Jorge Delgado <jorgeadm4@gmail.com>
2022-09-20 19:32:38 -0500

    Added mail and password labels, and text fields

---
+++ AppiOS/AppiOS/Controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  AppiOS
+//
+//  Created by Jorge Delgado on 20/09/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var EmailInput: UnderlinedTextField!
+    @IBOutlet weak var PasswordInput: UnderlinedTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
