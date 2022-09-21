3de821691e479ef65f1e09c2d7555cf6e04d93e6
Jorge Delgado <jorgeadm4@gmail.com>
2022-09-20 23:06:48 -0500

    Added sign in auth

--- AppiOS/AppiOS/Controllers/LoginViewController.swift
+++ AppiOS/AppiOS/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
@@ -13,11 +14,28 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var PasswordInput: UnderlinedTextField!
     @IBOutlet weak var LoginButon: UIButton!
     
+    @IBOutlet weak var EmailErrorLabel: UILabel!
+    @IBOutlet weak var PasswordErrorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         LoginButon.tintColor = UIColor(named: "Color1")
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = EmailInput.text, let password = PasswordInput.text {
+            Auth.auth().signIn(withEmail: email, password: password) {
+                authResult, error in
+                if let e = error {
+                    self.PasswordErrorLabel.text = e.localizedDescription
+                }
+                else {
+                    print(":)")
+                }
+            }
+        }
     }
     
 }
