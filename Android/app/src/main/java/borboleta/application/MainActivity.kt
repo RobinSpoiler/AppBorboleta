6be8e60e543fd706826e7c4a80229bacd4dda6c1
Karla Sanchez <ksoalianza@gmail.com>
2022-09-21 12:58:14 -0500

    Starting page

--- Android/app/src/main/java/borboleta/application/MainActivity.kt
+++ Android/app/src/main/java/borboleta/application/MainActivity.kt
@@ -2,9 +2,11 @@ package borboleta.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tec.a01423294.borboleta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
