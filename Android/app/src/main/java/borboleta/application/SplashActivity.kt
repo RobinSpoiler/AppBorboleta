ece1cebb3f61fe7a751ae8938193498e6f7036e8
Karla Sanchez <ksoalianza@gmail.com>
2022-09-27 18:26:58 -0500

    Splash screen demo

--- Android/app/src/main/java/borboleta/application/SplashActivity.kt
+++ Android/app/src/main/java/borboleta/application/SplashActivity.kt
@@ -1,22 +1,25 @@
 package borboleta.application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 @Suppress("DEPRECATION")
-class Login : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_splash)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
         )
-    }
 
+        Handler().postDelayed({
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }, 2000)
+    }
 }
