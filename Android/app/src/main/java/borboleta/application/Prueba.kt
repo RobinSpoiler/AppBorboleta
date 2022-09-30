4b3c40497e7483c683357e49db54150b56664aa2
JIsraP <a01423294@tec.mx>
2022-09-29 19:14:54 -0500

    Prueba login_registro

--- Android/app/src/main/java/borboleta/application/Prueba.kt
+++ Android/app/src/main/java/borboleta/application/Prueba.kt
@@ -19,11 +19,29 @@ class Prueba : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prueba)
         val b = findViewById<Button>(R.id.botonprueba)
+        val r = findViewById<Button>(R.id.register)
+        val t1 = findViewById<TextInputEditText>(R.id.texto1) as EditText
+        val t2 = findViewById<TextInputEditText>(R.id.texto2) as EditText
 
+        r.setOnClickListener {
+            auth.createUserWithEmailAndPassword(t1.text.toString(), t2.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("success", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        /*updateUI(user)*/
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("failure", "createUserWithEmail:failure", task.exception)
+                        /*Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)*/
+                    }
+                }
+        }
 
         b.setOnClickListener {
-            val t1 = findViewById<TextInputEditText>(R.id.texto1) as EditText
-            val t2 = findViewById<TextInputEditText>(R.id.texto2) as EditText
             Log.d("myTag", "This is my message");
 
             auth.signInWithEmailAndPassword(t1.text.toString(), t2.text.toString()).addOnCompleteListener(this) { task ->
