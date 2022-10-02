b316d1b68660c4432488aae9744a2e236a93fcf0
Jorge Delgado <jorgeadm4@gmail.com>
2022-10-01 20:04:47 -0500

    sc

--- AppiOS/AppiOS/Controllers/SignupPreferencesViewController.swift
+++ AppiOS/AppiOS/Controllers/SignupPreferencesViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class SignupPreferencesViewController: UIViewController {
 
+    @IBOutlet var Q1Options: [UIButton]!
+    var Q1Selected: Int? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
 
+    @IBAction func Q1OptionSelected(_ sender: UIButton) {
+        let selectedImg = UIImage(systemName: "checkmark.square.fill")
+        let unselectedImg = UIImage(systemName: "square")
+        
+        for option in Q1Options {
+            if sender.tag == option.tag {
+                option.setImage(selectedImg, for: .normal)
+                Q1Selected = sender.tag
+            }
+            else {
+                option.setImage(unselectedImg, for: .normal)
+            }
+        }
+    }
+    
 }
