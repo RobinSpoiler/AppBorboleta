e66916f02cf63247e040f52ae7901ee82d8cf5fb
Karla Sanchez <ksoalianza@gmail.com>
2022-10-09 11:47:44 -0500

    Retro connection with home

--- Android/app/src/main/java/borboleta/application/retro.kt
+++ Android/app/src/main/java/borboleta/application/retro.kt
@@ -1,19 +1,11 @@
 package borboleta.application
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class Retro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retro)
-
-        //IR A HOME O A PSICOLOGOS?
-        var btnHome = findViewById<Button>(R.id.btn_backmenu_retro)
-        btnHome.setOnClickListener(){
-            startActivity(Intent(this, Home::class.java))
-        }
     }
 }
