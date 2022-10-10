7e441338c1aac7045654a3e4cf267e99f6b79d28
Karla Sanchez <ksoalianza@gmail.com>
2022-10-09 19:15:38 -0500

    retro bug new commit

--- Android/app/src/main/java/borboleta/application/retro.kt
+++ Android/app/src/main/java/borboleta/application/retro.kt
@@ -1,19 +1,11 @@
 package borboleta.application
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class Retro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retro)
-
-        //REGRESO A REGISTRO 1
-        val btnMenu = findViewById<Button>(R.id.btn_backmenu_retro)
-        btnMenu.setOnClickListener{
-            startActivity(Intent(this, Home::class.java))
-        }
     }
 }
