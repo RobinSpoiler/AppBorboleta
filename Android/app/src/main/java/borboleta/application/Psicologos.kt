3848034c630fbb89e1562eec892aba2e13215ecb
Karla Sanchez <ksoalianza@gmail.com>
2022-10-09 21:12:20 -0500

    tried queries

--- Android/app/src/main/java/borboleta/application/Psicologos.kt
+++ Android/app/src/main/java/borboleta/application/Psicologos.kt
@@ -4,19 +4,30 @@ import android.content.Intent
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
 
 class Psicologos : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_psicologos)
+        // Access a Cloud Firestore instance from your Activity
+        val db = Firebase.firestore
+        val users = db.collection("users")
+        auth = Firebase.auth
+
 
         var navbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView_navbar)
         navbar.menu.findItem(R.id.heart_icon).isChecked = true;
@@ -42,6 +53,7 @@ class Psicologos : AppCompatActivity() {
         var storage = Firebase.storage
         var storageRef = storage.reference
         var imagesRef: StorageReference? = storageRef.child("images")
+///
 
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
 
@@ -52,10 +64,28 @@ class Psicologos : AppCompatActivity() {
             (getChildAt(0) as RecyclerView).overScrollMode =
                 RecyclerView.OVER_SCROLL_NEVER // Remove the scroll effect
         }
-        
+        val query = users.whereEqualTo("docData.preferences.p1", "2")
+
+        print(query.toString())
+        println("AQUI EMPIEZASDFSDF")
+        users
+            .whereEqualTo("docData.preferences.p1", "2")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d("retreived info", "${document.id} => ${document.data}")
+                    println("AAAAAAAAAAAAAA RETREIVED")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("AAAAAAAAAAAAAA NOT RETREIVED", "Error getting documents: ", exception)
+
+                println("AAAAAAAAAAAAAA NOOOOOO RETREIVED")
+            }
+        println("AQUI TERMINAAAA")
         //ESTE ES EL ARRAY CON LA INFORMACION
         val demoData = arrayListOf(
-            "Esta sería la primera",
+            "Esta sería la ñam",
             "Esta la segunda",
             "Praesent TERCERA nibh.",
             "Aliquam sodales imperdiet augue at consectetur. Suspendisse dui mauris, tincidunt non auctor quis, facilisis et tellus.",
