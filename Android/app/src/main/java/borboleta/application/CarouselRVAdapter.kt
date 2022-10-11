1080c41aee3186a545bd9b89f9f205c357c81245
Karla Sanchez <ksoalianza@gmail.com>
2022-10-11 03:21:27 -0500

    Matches VIEW carousel

--- Android/app/src/main/java/borboleta/application/CarouselRVAdapter.kt
+++ Android/app/src/main/java/borboleta/application/CarouselRVAdapter.kt
@@ -1,29 +1,40 @@
 package borboleta.application
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
 
+class SliderAdapter : ListAdapter<Int,RecyclerView.ViewHolder>(DiffCallback()) {
 
-class CarouselRVAdapter(private val carouselDataList: ArrayList<String>) :
-    RecyclerView.Adapter<CarouselRVAdapter.CarouselItemViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val ivWallpaper: ImageView = itemView.findViewById(R.id.ivWallpaper)
+        fun onBind(wallpaper: Int){
+            Glide.with(ivWallpaper)
+                .load(wallpaper)
+                .into(ivWallpaper)
+        }
+    }
+    private class DiffCallback : DiffUtil.ItemCallback<Int>() {
+        override fun areItemsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
 
-    class CarouselItemViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselItemViewHolder {
-        val viewHolder = LayoutInflater.from(parent.context).inflate(R.layout.items_carousel, parent, false)
-        return CarouselItemViewHolder(viewHolder)
+        override fun areContentsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
     }
 
-    override fun onBindViewHolder(holder: CarouselItemViewHolder, position: Int) {
-        val textView = holder.itemView.findViewById<TextView>(R.id.textview)
-        textView.text = carouselDataList[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.items_carousel, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        return carouselDataList.size
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item =getItem(position)
+        val hold = holder as ViewHolder
+        hold.onBind(item)
     }
+
 
 }
--- Android/app/src/main/java/borboleta/application/Psicologos.kt
+++ Android/app/src/main/java/borboleta/application/Psicologos.kt
@@ -1,71 +1,27 @@
 package borboleta.application
 
-import android.app.ProgressDialog
+
 import android.content.Intent
-import android.content.res.Resources
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
-import borboleta.application.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
-import java.io.File
+import java.lang.Math.abs
 
 class Psicologos : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var vpSlider: ViewPager2
+    private lateinit var sliderAdapter: SliderAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val storagas = Firebase.storage
-        val storageRef = storagas.reference
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_psicologos)
-        // Access a Cloud Firestore instance from your Activity
-        val db = Firebase.firestore
-        val users = db.collection("users")
-        auth = Firebase.auth
 
-
-        /*val imageName = binding.etimageId.text.toString()*/
-        val progressDialog = ProgressDialog(this)
-        progressDialog.setMessage("Ahi vamos, jalale")
-        progressDialog.setCancelable(false)
-        progressDialog.show()
-
-        var ref = FirebaseStorage.getInstance().reference.child("profilePics/puser1@gmail.com.png")
-
-        val localfile = File.createTempFile("tempimage", "png")
-        ref.getFile(localfile).addOnSuccessListener {
-            if(progressDialog.isShowing){
-                progressDialog.dismiss()
-            }
-            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
-            binding.imageView.setImageBitmap(bitmap)
-
-        }.addOnFailureListener{
-            if(progressDialog.isShowing){
-                progressDialog.dismiss()
-            }
-            Toast.makeText(this, "Fuck, failed NO IMAGE AAA", Toast.LENGTH_SHORT)
-        }
-
-
+        initView()
 
         var navbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView_navbar)
-        navbar.menu.findItem(R.id.heart_icon).isChecked = true;
+        navbar.menu.findItem(R.id.home_icon).isChecked = true;
         navbar.setOnItemSelectedListener{ item ->
             when(item.itemId) {
                 R.id.home_icon -> {
@@ -82,54 +38,36 @@ class Psicologos : AppCompatActivity() {
             }
         }
 
-
-
-        //Create storage reference
-        var storage = Firebase.storage
-        /*var storageRef = storage.reference*/
-        var imagesRef: StorageReference? = storageRef.child("images")
-///
-
-        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
-
-        viewPager.apply {
-            clipChildren = false  // No clipping the left and right items
-            clipToPadding = false  // Show the viewpager in full width without clipping the padding
-            offscreenPageLimit = 3  // Render the left and right items
-            (getChildAt(0) as RecyclerView).overScrollMode =
-                RecyclerView.OVER_SCROLL_NEVER // Remove the scroll effect
-        }
-        val query = users.whereEqualTo("docData.preferences.p1", "2")
-
-        println("AQUI EMPIEZASDFSDF")
-        users
-            .whereEqualTo("preferences.p1", 2)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    Log.d("retreived info", "${document.id} => ${document.data}")
-                    println("AAAAAAAAAAAAAA RETREIVED")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w("AAAAAAAAAAAAAA NOT RETREIVED", "Error getting documents: ", exception)
-
-                println("AAAAAAAAAAAAAA NOOOOOO RETREIVED")
-            }
-        println("AQUI TERMINAAAA")
-        //ESTE ES EL ARRAY CON LA INFORMACION
-        val demoData = arrayListOf(
-            "Esta sería la ñam",
-            "Esta la segunda",
-            "Praesent TERCERA nibh.",
-            "Aliquam sodales imperdiet augue at consectetur. Suspendisse dui mauris, tincidunt non auctor quis, facilisis et tellus.",
-            "Ut non tincidunt neque, et sodales ligula. Quisque interdum in dui sit amet sagittis. Curabitur erat magna, maximus quis libero quis, dapibus eleifend orci."
-        )
-
-        viewPager.adapter = CarouselRVAdapter(demoData)
-
-        val compositePageTransformer = CompositePageTransformer()
-        compositePageTransformer.addTransformer(MarginPageTransformer((40 * Resources.getSystem().displayMetrics.density).toInt()))
-        viewPager.setPageTransformer(compositePageTransformer)
     }
+
+    private fun initView(){
+        vpSlider = findViewById(R.id.vpSlider)
+        //num de photos que se "ven" en pantalla
+        vpSlider.offscreenPageLimit = 3
+        vpSlider.setPageTransformer(getTransformation())
+        sliderAdapter = SliderAdapter()
+        vpSlider.adapter = sliderAdapter
+        sliderAdapter.submitList(getWallpapers())
+    }
+    //esta es como la animación de los psicólogos para que las cards se hagan pequeñas si estan al lado
+    private fun getTransformation():CompositePageTransformer{
+        val transform = CompositePageTransformer()
+        transform.addTransformer(MarginPageTransformer(30))
+        transform.addTransformer{page, position ->
+            page.scaleY = 0.85f + (1 - abs(position)) * 0.15f
+        }
+        return transform
+    }
+
+    //Este es el arreglo donde van las imágenes
+    private fun getWallpapers(): List<Int>{
+        val data = arrayListOf<Int>()
+        data.add(R.drawable.spotify)
+        data.add(R.drawable.success)
+        return data
+    }
+
+
+
+
 }
