7b5896b1d2b7c4a6f8191aa8a8e55d928be971eb
JIsraP <a01423294@tec.mx>
2022-10-16 22:15:40 -0500

    Chat List v1.2

--- Android/app/src/main/java/borboleta/application/ActiveChats.kt
+++ Android/app/src/main/java/borboleta/application/ActiveChats.kt
@@ -1,33 +1,47 @@
 package borboleta.application
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.getField
+
 
 class ActiveChats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_active_chats)
 
-        val dynamicCardview = CardView(this)
-        val dynamicTextview = TextView(this)
-        val dynamicImageview = ImageView(this)
+        val db = FirebaseFirestore.getInstance()
+
+        val docRef = db.collection("users").document("K53rfbNnTJctTAyhID7D").get().addOnSuccessListener { doc ->
+            var message = doc.getField<String>("0.message")
+            println(message)
+            /*for(doc in documents){
+                val dynamicCardview = CardView(this)
+                val dynamicTextview = TextView(this)
+                val dynamicImageview = ImageView(this)
 
 
-        var layout = findViewById<LinearLayout>(R.id.mainlayout)
-// add TextView to LinearLayout
-        layout.addView(dynamicCardview)
-        layout.addView(dynamicImageview)
-        layout.addView(dynamicTextview)
+                var layout = findViewById<LinearLayout>(R.id.mainlayout)
 
-        dynamicCardview.
-        dynamicImageview.setImageResource(R.drawable.logomariposa)
-        dynamicImageview.layoutParams= LinearLayout.LayoutParams(100, 100)
+                dynamicImageview.setImageResource(R.drawable.logomariposa)
+                dynamicImageview.layoutParams= LinearLayout.LayoutParams(150, 100)
+                dynamicImageview.x = 80F
+
+                var name : String = doc.getField<String>("data.name").toString()
+                dynamicTextview.text = name
+                dynamicTextview.x = 220F
+
+                dynamicCardview.layoutParams = LinearLayout.LayoutParams(2000, 200)
+                dynamicCardview.addView(dynamicImageview)
+                dynamicCardview.addView(dynamicTextview)
+
+                layout.addView(dynamicCardview)
+
+            }*/
+
+        }
 
 
-        dynamicTextview.text = "Dynamically added text"
     }
 }
