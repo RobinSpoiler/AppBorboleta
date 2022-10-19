47a409496590ed8d8a9aa3066f5121ca687b5a6d
JIsraP <a01423294@tec.mx>
2022-10-19 15:42:11 -0500

    Functional profile interface with logout and editable fields(update in BD)

--- Android/app/src/main/java/borboleta/application/Profile.kt
+++ Android/app/src/main/java/borboleta/application/Profile.kt
@@ -1,18 +1,19 @@
 package borboleta.application
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 
 class Profile : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -21,6 +22,10 @@ class Profile : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         //DATABASE
         var db = Firebase.firestore
+        auth = Firebase.auth
+        val storage = FirebaseStorage.getInstance()
+
+        //Nav var
         var navbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView_navbar)
         navbar.menu.findItem(R.id.profile_icon).isChecked = true;
         navbar.setOnItemSelectedListener { item ->
@@ -52,46 +57,66 @@ class Profile : AppCompatActivity() {
 
 
         //Funcionalidad
+        var profilePic = findViewById<ImageView>(R.id.profile_picture)
+        var editFields = findViewById<Button>(R.id.edit_button_profile)
+        var nombre = findViewById<TextInputEditText>(R.id.profile_name)
+        var number = findViewById<TextInputEditText>(R.id.profile_number)
+        var account = findViewById<TextInputEditText>(R.id.profile_account_type)
+        var age = findViewById<TextInputEditText>(R.id.profile_age)
+
         var botonexit = findViewById<ImageButton>(R.id.logout_button_profile)
         botonexit.setOnClickListener(){
             Firebase.auth.signOut()
             startActivity(Intent(this, Login::class.java))
         }
 
-
         val user = auth.currentUser
 
-        var editFields = findViewById<Button>(R.id.edit_button_profile)
-        var nombre = findViewById<TextInputEditText>(R.id.profile_name)
-        var number = findViewById<TextInputEditText>(R.id.profile_number)
-        var account = findViewById<TextInputEditText>(R.id.profile_account_type)
-        var age = findViewById<TextInputEditText>(R.id.profile_age)
-        var flag = false
-        editFields.setOnClickListener(){
-            if(!flag){
-                editFields.text = "GUARDAR"
-                nombre.isEnabled = true
-                number.isEnabled = true
-                age.isEnabled = true
-                flag = true
-                //hacer cosas locas de update
-
-                var docRef = db.collection("users").document("${user?.email}").update(
-                    mapOf("name" to nombre.text.toString())
+        storage.reference.child("profilePics/${user?.email}.png").downloadUrl.addOnSuccessListener { url ->
+            GlideApp.with(this)
+                .load(url.toString())
+                .apply(
+                    RequestOptions()
+                    .circleCrop()
                 )
+                .into(profilePic)
+        }
 
-// Update the timestamp field with the value from the server
+        //Get info from user
+        db.collection("users").document("${user?.email}").get().addOnSuccessListener { doc ->
+            //Display data from DB
+            var data = doc.get("data") as? Map<String, String>
+            nombre.setText(data?.get("name"))
+            number.setText(data?.get("phone"))
+            account.setText(data?.get("accountType"))
+            age.setText(data?.get("birthday"))
 
+            //Edit Save Button function
+            var flag = false
+            editFields.setOnClickListener(){
+                if(!flag){
+                    editFields.text = "Guardar"
+                    nombre.isEnabled = true
+                    number.isEnabled = true
+                    age.isEnabled = true
+                    flag = true
 
+                }
+                else if(flag){
+                    editFields.text = "Editar"
+                    nombre.isEnabled = false
+                    number.isEnabled = false
+                    age.isEnabled = false
+
+                    //hacer cosas locas de update
+                    db.collection("users").document("${user?.email}").update(mapOf(
+                        "data.name" to nombre.text.toString(),
+                        "data.phone" to number.text.toString(),
+                        "data.birthday" to age.text.toString()
+                    ))
+                }
 
             }
-            else if(flag){
-                editFields.text = "edit bish"
-                nombre.isEnabled = false
-                number.isEnabled = false
-                age.isEnabled = false
-            }
-
         }
     }
 }
